72787c5914f040c6f5beacdb0b11740b4c9c52ff
Cody Hatfield <cody.hatfield@me.com>
2021-06-14 10:07:14 -0700

    Remove matic-mainnet-sentry

--- default.nix
+++ default.nix
@@ -5,6 +5,5 @@ in {
     heimdall = pkgs.callPackage ./pkgs/heimdall/default.nix { };
     bor = pkgs.callPackage ./pkgs/bor/default.nix { };
     openethereum = pkgs.callPackage ./pkgs/openethereum/default.nix { };
-    matic-mainnet-sentry = pkgs.callPackage ./pkgs/matic-mainnet-sentry/default.nix { };
 }
 
