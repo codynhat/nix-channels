7703fb22288f0c5e9344027f4210368516a81756
Cody Hatfield <cody.hatfield@me.com>
2021-06-23 22:56:17 -0700

    Add graph-node package

--- default.nix
+++ default.nix
@@ -5,5 +5,6 @@ in {
     heimdall = pkgs.callPackage ./pkgs/heimdall/default.nix { };
     bor = pkgs.callPackage ./pkgs/bor/default.nix { };
     openethereum = pkgs.callPackage ./pkgs/openethereum/default.nix { };
+    graph-node = pkgs.callPackage ./pkgs/graph-node/default.nix { };
 }
 
---
+++ pkgs/graph-node/default.nix
@@ -0,0 +1,23 @@
+{ fetchFromGitHub, lib, rustPlatform, pkgs }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "graph-node";
+  version = "0.23.1";
+  rev = "v${version}";
+
+  buildInputs = [ pkgs.libiconv pkgs.postgresql_11 ];
+
+  src = fetchFromGitHub {
+    owner = "graphprotocol";
+    repo = pname;
+    inherit rev;
+    sha256 = "0r78z4b07vqlcdvfwcal96r8k7hs80dynxp6rg0zl2r6b0nvn3m5";
+  };
+
+  cargoSha256 = "0wj9prdaahi77iybwg24ywl2af31nymmvaqd2f5azawdb1057l3s";
+
+  meta = with lib; {
+    description = "Graph Node indexes data from blockchains such as Ethereum and serves it over GraphQL";
+    homepage = "https://github.com/graphprotocol/graph-node";
+  };
+}
