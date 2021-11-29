66ba8bfdb4302f58faa4c492a755e0cbc2bcc50c
Cody Hatfield <cody.hatfield@me.com>
2021-11-29 14:54:40 -0800

    Upgrade openethereum to 3.3.0

--- pkgs/openethereum/default.nix
+++ pkgs/openethereum/default.nix
@@ -1,23 +1,50 @@
-{ stdenv, fetchzip, lib }:
+{ lib
+, fetchFromGitHub
+, rustPlatform
+, cmake
+, llvmPackages
+, openssl
+, pkg-config
+, stdenv
+, systemd
+, darwin
+}:
 
-stdenv.mkDerivation rec {
-  name = "openethereum";
-  version = "3.2.5";
-  src = fetchzip {
-    url = "https://github.com/openethereum/openethereum/releases/download/v${version}/openethereum-macos-v${version}.zip";
-    sha256 = "0l6kmm2d80h32c03sbbl5rvgr0w1piq4452cibq1pyx44vvp3hpx";
-    stripRoot = false;
+rustPlatform.buildRustPackage rec {
+  pname = "openethereum";
+  version = "3.3.0";
+
+  src = fetchFromGitHub {
+    owner = "openethereum";
+    repo = "openethereum";
+    rev = "v${version}";
+    sha256 = "1qs1x2nrivg1q4a85m0sg9wjrhxyqirz3nq17nmxszyadaa2az38";
   };
 
-  installPhase = ''
-    install -m755 -D openethereum $out/bin/openethereum
+  cargoSha256 = "10y7515xk8a43kywpcdk57p3z45y89xmxr6d87lk9cjx7i5x2s8l";
+
+  nativeBuildInputs = [ cmake pkg-config ];
+
+  buildInputs = [ openssl ]
+    ++ lib.optionals stdenv.isLinux [ systemd ]
+    ++ lib.optionals stdenv.isDarwin [ darwin.Security ];
+
+  buildFeatures = [ "final" ];
+
+  # Fix tests by preventing them from writing to /homeless-shelter.
+  preCheck = ''
+    export HOME=$(mktemp -d)
   '';
+
+  # Exclude some tests that don't work in the sandbox
+  # - Nat test requires network access
+  checkFlags = "--skip configuration::tests::should_resolve_external_nat_hosts";
 
   meta = with lib; {
     description = "Fast, light, robust Ethereum implementation";
     homepage = "http://parity.io/ethereum";
     license = licenses.gpl3;
-    maintainers = with maintainers; [ akru xrelkd ];
-    platforms = platforms.unix;
+    maintainers = with maintainers; [ akru ];
+    platforms = lib.platforms.unix;
   };
 }
